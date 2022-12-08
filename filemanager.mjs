f0b644b9538cd28922454dc6207a205bdcf36193
Eugen <4workacc@gmail.com>
2022-12-08 13:32:58 +0300

    feat: fs ls command ready

--- filemanager.mjs
+++ filemanager.mjs
@@ -1,7 +1,9 @@
 import process from 'process';
 import os from 'os';
+import * as fs from 'fs';
 
 import { displayWorkDir } from './utils.mjs';
+import { clear } from 'console';
 
 const userName = process.argv[2];
 let curUserDir = os.homedir();
@@ -10,12 +12,13 @@ console.log(`Welcome to the File Manager, ${userName}!`);
 displayWorkDir(curUserDir);
 
 process.stdin.on('data', data => {
-    let command = data.toString().trim();    
+    let command = data.toString().trim();
     switch (command) {
         case '.exit':
             console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
             process.stdin.pause();
             break;
+        // OS Section
         case 'os --homedir':
             console.log(os.homedir())
             break;
@@ -25,8 +28,34 @@ process.stdin.on('data', data => {
         case 'os --cpus':
             console.log(os.cpus());
             break;
-        case 'os --architecture': 
+        case 'os --architecture':
             console.log(os.arch());
+            break;
+        case 'os --EOL':
+            console.log(os.EOL);
+            break;
+        //FS Section
+        case 'ls':
+            let dirs = fs.readdirSync(curUserDir);
+            let dispDirs = [];
+            dirs.map((obj) => {
+                let isDirectory = fs.lstatSync(`${curUserDir}/${obj}`).isDirectory();
+                dispDirs.push({
+                    "name": obj,
+                    "type": isDirectory ? 'file' : 'directory'
+                });
+            })
+
+            dispDirs.sort((a, b) => (a.type > b.type) ? 1 : -1)
+
+            console.table(
+                dispDirs.map(obj => {
+                    return {
+                        'name': obj.name,
+                        'type': obj.type
+                    }
+                })
+            );
             break;
         default:
             console.log('Invalid input')
@@ -38,6 +67,6 @@ process.on('SIGINT', () => {
     process.stdin.pause();
 })
 
-process.on('error', ()=>{
+process.on('error', () => {
     console.log('Operation failed')
 })
