aac78b4f942b26a5d155dacbda9a138edce23c99
Eugen <4workacc@gmail.com>
2022-12-08 12:04:56 +0300

    feat: fix username sending to script

--- filemanager.mjs
+++ filemanager.mjs
@@ -1,18 +1,21 @@
 import process from 'process';
 
-process.stdin.on('data', data=>{
+const userName = process.argv[2];
+console.log(`Welcome to the File Manager, ${userName}!`)
+process.stdin.on('data', data => {
     let command = data.toString().trim();
-    console.log(command === '.exit')    
-    switch ( command ) {
-        case '.exit' :           
-           console.log('Goodbay, ');
-           process.stdin.pause()
-        default :
+    console.log(command === '.exit')
+    switch (command) {
+        case '.exit':
+            console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+            process.stdin.pause()
+        default:
             console.log('Unkmnown command')
     }
 })
 
-process.on('SIGINT', ()=>{
-    console.log('Goodbay, ');
+process.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
     process.stdin.pause();
 })
+
