24c7aea00aabdedf7750a6991c39bd415972c850
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-19 16:43:28 +0300

    refactor: fix extra simbols with decompression

--- src/compress_utils.mjs
+++ src/compress_utils.mjs
@@ -6,8 +6,8 @@ export const compressFile = async (curUserDir, filePath, gzipPath) => {
         if (stat === undefined) { console.log('Error: start file dont exist') } else {
             fs.stat(`${curUserDir}\\${gzipPath}`, (err, stat2) => {
                 if (stat2 === undefined) {
-                    const fil = fs.createReadStream(`${curUserDir}\\${filePath}`, {encoding: 'utf-8'});
-                    const zip = fs.createWriteStream(`${curUserDir}\\${gzipPath}`, {encoding: 'utf-8'});
+                    const fil = fs.createReadStream(`${curUserDir}\\${filePath}`);
+                    const zip = fs.createWriteStream(`${curUserDir}\\${gzipPath}`);
                     const zi = zlib.createBrotliCompress();
                     fil.pipe(zi).pipe(zip);
                     console.log(`File ${filePath} compressed to ${gzipPath}`);
@@ -25,9 +25,9 @@ export const decompressFile = (curUserDir, gzipPath, filePath) => {
         if (stat === undefined) { console.log('Error: zip file dont exist') } else {
             fs.stat(`${curUserDir}\\${filePath}`, (err, stat2) => {
                 if (stat2 === undefined) {
-                    const fil = fs.createReadStream(`${gzipPath}`, {encoding: 'utf-8'});
-                    const zip = fs.createWriteStream(`${filePath}`, { encoding: 'utf-8' });
-                    const zi = zlib.createBrotliCompress();
+                    const fil = fs.createReadStream(`${gzipPath}`);
+                    const zip = fs.createWriteStream(`${filePath}`);
+                    const zi = zlib.createBrotliDecompress();
                     fil.pipe(zi).pipe(zip);
                     console.log(`File ${gzipPath} decompressed to ${filePath}`);
                 }
