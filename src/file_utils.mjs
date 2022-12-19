bc223e07acbde6e8c7cf54b870f716c56bf6b007
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-19 13:29:39 +0300

    refactor: copy

--- src/file_utils.mjs
+++ src/file_utils.mjs
@@ -19,7 +19,7 @@ export const cat_util = (path) => {
 export const add_file_util = (path, curUserDir) => {
     fs.access(path, fs.F_OK, (err) => {
         if (err) {
-            fs.writeFile(path,'', ()=>{});
+            fs.writeFile(path, '', () => { });
             console.log('File succesfully created');
             console.log(`You are currently in ${curUserDir}`)
         }
@@ -27,23 +27,63 @@ export const add_file_util = (path, curUserDir) => {
             console.log('Error: file already exist');
             console.log(`You are currently in ${curUserDir}`)
         }
-    });    
+    });
 }
 
 export const delete_util = (curUserDir, param) => {
     if (!param) { console.log('Error: file should be named') }
-    else {     
+    else {
         fs.access(`${curUserDir}\\${param}`, fs.F_OK, (err) => {
-            if ( err ) {
+            if (err) {
                 console.log('Error: file dont exist')
             }
             else {
                 fs.unlink(`${curUserDir}\\${param}`, (err) => {
-                    if (err) { console.log ('Delete file error') }
+                    if (err) { console.log('Delete file error') }
                     else console.log(`File ${param} deleted successfully`)
                 })
             }
-        })       
+        })
     }
-    console.log(`You are currently in ${curUserDir}`);    
+    console.log(`You are currently in ${curUserDir}`);
+}
+
+export const rename_util = (curUserDir, param, param1) => {
+    fs.access(`${curUserDir}\\${param}`, fs.F_OK, (err0) => {
+        if (err0) console.log('Error: start file dont exist')
+        else {
+            fs.access(`${curUserDir}\\${param1}`, fs.F_OK, (err1) => {
+                if (err1) console.log('Error: file with newName already exist')
+                else {
+                    fs.rename(`${curUserDir}\\${param}`, `${curUserDir}\\${param1}`, (err2) => {
+                        if (err2) {
+                            console.log('Error: some error at renaming');
+                        }
+                        else {
+                            console.log('File rename succesfully');
+                        }
+                    })
+                }
+            })
+
+        }
+    })
+    console.log(`You are currently in ${curUserDir}`)
+};
+
+export const copy_util = (curUserDir, path, newPath) => {
+    fs.lstat(`${curUserDir}\\${newPath}`, (err, start) => {
+        if ( err ) { console.log('Error: target directory dont exist')} 
+        else {
+            fs.access(`${curUserDir}\\${path}`, fs.F_OK, (err1)=>{
+                if ( err1 ) { console.log('Error: file dont exist')} 
+                else {
+                    let rSteam = fs.createReadStream(`${curUserDir}\\${path}`);
+                    let wStream = fs.createWriteStream(`${curUserDir}\\${newPath}\\${path}`);
+                    rSteam.pipe(wStream);
+                    console.log(`File ${path} copyed to folder ${newPath}`)
+                }
+            })
+        }
+    } )   
 }
--- src/filemanager.mjs
+++ src/filemanager.mjs
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import * as crypto from 'crypto';
 import { compressFile, decompressFile, displayWorkDir, readFile } from './utils.mjs';
 import { cd_util, ls_util, up_util } from './fs_utils.mjs';
-import { add_file_util, cat_util, delete_util } from './file_utils.mjs';
+import { add_file_util, cat_util, copy_util, delete_util, rename_util } from './file_utils.mjs';
 
 const userName = process.platform === 'win32' ? process.argv[3] : process.argv[2];
 let curUserDir = os.homedir();
@@ -80,43 +80,14 @@ process.stdin.on('data', data => {
             if (!commandLine[1] || !commandLine[2]) {
                 console.log('command pattern is rn path_to_file new_filename')
             }
-            else {
-                if (!fs.existsSync(`${curUserDir}\\${commandLine[1]}`) || fs.existsSync(`${curUserDir}\\${commandLine[2]}`)) {
-                    console.log('Error: file not exits or rename file already exist')
-                }
-                else {
-                    let isErr = false;
-                    fs.renameSync(`${curUserDir}\\${commandLine[1]}`, `${curUserDir}\\${commandLine[2]}`, (err) => {
-                        if (err) {
-                            isErr = true;
-                        }
-                    });
-                    isErr ? console.log('Rename error') : console.log(`File ${curUserDir}\\${commandLine[1]} successfully rename to ${curUserDir}\\${commandLine[2]}`)
-                }
-            };
-            console.log(`You are currently in ${curUserDir}`)
+            else rename_util(curUserDir, commandLine[1], commandLine[2]);
             break;
         case 'cp':
             if (!commandLine[1] || !commandLine[2]) {
                 console.log('Error: command pattern is cp path_to_file path_to_new_directory')
             }
             else {
-                let isFileExist = fs.existsSync(`${curUserDir}\\${commandLine[1]}`);
-                let isDirExist = true;
-                try {
-                    fs.lstatSync(`${curUserDir}\\${commandLine[2]}`).isDirectory();
-                } catch (err) {
-                    isDirExist = false;
-                }
-                if (!isFileExist || !isDirExist) {
-                    console.log(`Error: file dont exist or target folder dont exist`);
-                }
-                else {
-                    let rSteam = fs.createReadStream(`${curUserDir}\\${commandLine[1]}`);
-                    let wStream = fs.createWriteStream(`${curUserDir}\\${commandLine[2]}\\${commandLine[1]}`);
-                    rSteam.pipe(wStream);
-                    console.log(`File ${commandLine[1]} copyed to folder ${commandLine[2]}`)
-                }
+               copy_util(curUserDir, commandLine[1], commandLine[2]);
             }
             console.log(`You are currently in ${curUserDir}`)
             break;
